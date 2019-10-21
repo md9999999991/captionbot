70bcd44fed14c4c95e94647c21f511a030d6d9a7
Mohit Dhawan <39364306+md9999999991@users.noreply.github.com>
2019-10-21 13:03:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='captionbot',
       version='0.1.4',
       description='Simple API wrapper for https://www.captionbot.ai/',
-      url='http://github.com/md9999999991/captionbot',
-      author='Mohit Dhawan',
-      author_email='md9999999991@gmail.com',
+      url='http://github.com/krikunts/captionbot',
+      author='Tatiana Krikun',
+      author_email='krikunts@gmail.com',
       license='MIT',
       packages=['captionbot'],
       install_requires=[
